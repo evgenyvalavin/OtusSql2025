d712345cac00fb665a8cb88bb89ee9dd16a11853
Evgeny Valavin <evgeny.valavin@outlook.com>
2026-04-25 15:43:07 +0300

    add vacuum

--- scripts/performance_monitoring.sql
+++ scripts/performance_monitoring.sql
@@ -20,3 +20,21 @@ SELECT * FROM pg_stat_activity;
 
 -- 4. Отобразить план запроса через: EXPLAIN ANALYZE
 EXPLAIN ANALYZE SELECT * FROM booking_platform.UpcomingTrips;
+
+
+-- 5.
+-- VACUUM (и AUTOVACUUM) помогает очистить страницы от “мёртвых” строк
+-- Следить за числом мёртвых строк в таблице можно по представлению:
+-- n_dead_tup	Number of dead (not yet vacuumed) rows
+-- n_live_tup	Number of live rows
+-- last_autovacuum	Timestamp of last autovacuum run
+-- last_vacuum	Timestamp of last manual VACUUM
+SELECT 
+    schemaname,
+    relname AS table_name,
+    n_dead_tup AS dead_tuples,
+    n_live_tup AS live_tuples,
+    last_vacuum,
+    last_autovacuum
+FROM pg_catalog.pg_stat_all_tables
+ORDER BY n_dead_tup DESC;
